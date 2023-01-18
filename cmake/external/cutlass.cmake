1a0626c73ba3b20bce710ec595854ac9a3af8cfa
Tianlei Wu <tlwu@microsoft.com>
2023-01-18 18:13:30 +0000

    fix cmake

--- cmake/external/cutlass.cmake
+++ cmake/external/cutlass.cmake
@@ -1,20 +1,17 @@
-set(cutlass_URL https://github.com/nvidia/cutlass.git)
-set(cutlass_TAG 8b42e751c63ba219755c8ed91af5f6ec1ecc1ee6)
-
-set(PATCH ${PROJECT_SOURCE_DIR}/patches/cutlass/41_fused_multi_head_atten_BMHK.patch)
-
+#set(PATCH ${PROJECT_SOURCE_DIR}/patches/cutlass/41_fused_multi_head_atten_BMHK.patch)
 include(FetchContent)
 FetchContent_Declare(cutlass
-  GIT_REPOSITORY ${cutlass_URL}
-  GIT_TAG        ${cutlass_TAG}
-  PATCH_COMMAND  git apply --reverse --check ${PATCH} || git apply --ignore-space-change --ignore-whitespace ${PATCH}
+  GIT_REPOSITORY https://github.com/nvidia/cutlass.git
+  GIT_TAG        8b42e751c63ba219755c8ed91af5f6ec1ecc1ee6
+  #PATCH_COMMAND  git apply --ignore-space-change --ignore-whitespace --whitespace=nowarn ${PATCH}
 )
 
 FetchContent_GetProperties(cutlass)
 if(NOT cutlass_POPULATED)
   FetchContent_Populate(cutlass)
 
-  target_include_directories(onnxruntime_cutlass_includes INTERFACE
-    ${cutlass_SOURCE_DIR}/include
-    ${cutlass_SOURCE_DIR}/examples)
+  #add_subdirectory(${cutlass_SOURCE_DIR} ${cutlass_BINARY_DIR} EXCLUDE_FROM_ALL)
+  # target_include_directories(onnxruntime_cutlass_includes PRIVATE
+  #   ${cutlass_SOURCE_DIR}/include
+  #   ${cutlass_SOURCE_DIR}/examples)
 endif()
