c22d7f911e679f0134e1704ad7d87e458f612067
Camden Harris <camdenharris@gmail.com>
2024-07-24 11:37:11 -0700

    desktop/browser/mobile graphic

--- src/routes/generative-ai/sectionone.svelte
+++ src/routes/generative-ai/sectionone.svelte
@@ -1,3 +1,33 @@
-<div class="container">
-    section one
+<script>
+</script>
+
+<div class="container p-20">
+	<div
+		class="grid grid-cols-1 gap-4 bg-primary justify-items-center items-center p-4 lg:grid-cols-3 rounded-3xl"
+	>
+		<div class="mx-auto">
+			<img
+				class="h-full mx-auto"
+				src="src/images/desktop_example_2k.png"
+				alt="a monitor clipart showing an interaction the the Phi-3 SLM in a CLI"
+			/>
+			<p class="text-center mt-4 text-2xl">Desktop</p>
+		</div>
+		<div class="mx-auto">
+			<img
+				class="h-full mx-auto"
+				src="src/images/browser_example_2k.png"
+				alt="a laptop clipart showing an interaction the the Phi-3 SLM in a browser"
+			/>
+			<p class="text-center mt-4 text-2xl">Browser</p>
+		</div>
+		<div class="mx-auto">
+			<img
+				class="h-full"
+				src="src/images/mobile_example_2k.png"
+				alt="a phone clipart showing an interaction with the Phi-3 SLM in a mobile app"
+			/>
+			<p class="text-center mt-4 text-2xl">Mobile</p>
+		</div>
+	</div>
 </div>
